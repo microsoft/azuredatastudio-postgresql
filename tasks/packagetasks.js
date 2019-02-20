3aecb3c70b98dbbb67dc60fccea14f162ea78268
Anup Kamath <ankam@microsoft.com>
2019-02-19 16:25:11 -0800

    Updating license and TPN

--- tasks/packagetasks.js
+++ tasks/packagetasks.js
@@ -38,6 +38,7 @@ function doPackageSync(packageName) {
     var vsceArgs = [];
     vsceArgs.push('vsce');
     vsceArgs.push('package'); // package command
+    vsceArgs.push('--yarn'); // to use yarn list instead on npm list
 
     if (packageName !== undefined) {
         vsceArgs.push('-o');
