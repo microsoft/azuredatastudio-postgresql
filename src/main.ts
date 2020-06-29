63be2365f8ce053ee5dabf8ec08075da338635f2
Kevin Mao <t-kemao@microsoft.com>
2020-06-29 15:15:03 -0400

    rename to ossdbtoolsservice

--- src/main.ts
+++ src/main.ts
@@ -141,7 +141,7 @@ function generateServerOptions(executablePath: string): ServerOptions {
 		let useLocalSource = config["useDebugSource"];
 		if (useLocalSource) {
 			let localSourcePath = config["debugSourcePath"];
-			let filePath = path.join(localSourcePath, "ostoolsservice/ostoolsservice_main.py");
+			let filePath = path.join(localSourcePath, "ossdbtoolsservice/ossdbtoolsservice_main.py");
 			process.env.PYTHONPATH = localSourcePath;
 			serverCommand = process.platform === 'win32' ? 'python' : 'python3';
 
