b771dc18b81c8024d6920b1812b0f36a816c948e
Kevin Mao <t-kemao@microsoft.com>
2020-07-27 15:15:21 -0400

    Rename crash and project output message

--- src/constants.ts
+++ src/constants.ts
@@ -10,8 +10,8 @@ const localize = nls.loadMessageBundle();
 
 export const serviceName = localize('extension.serviceName', 'ossdbToolsService');
 export const providerId = localize('extension.providerId', 'PGSQL');
-export const serviceCrashMessage = localize('extension.serviceCrashMessage', 'Oss DB Tools Service component exited unexpectedly. Please restart Azure Data Studio.');
+export const serviceCrashMessage = localize('extension.serviceCrashMessage', 'OSS DB Tools Service component exited unexpectedly. Please restart Azure Data Studio.');
 export const serviceCrashButton = localize('extension.serviceCrashButton', 'View Known Issues');
 export const serviceCrashLink = localize('extension.serviceCrashLink', 'https://github.com/microsoft/pgtoolsservice/issues?q=is%3Aopen+is%3Aissue+label%3Aknown-issues');
-export const projectOutputChannel = localize('extension.projectOutputChannel', 'ossdbProject');
+export const projectOutputChannel = localize('extension.projectOutputChannel', 'pgSQLProject');
 export const templateDoesNotExistMessage = localize('extension.templateDoesNotExistMessage', 'No templates matched the input template name: pgproj.');
