ed57f500636036748673ba137cacffb66fa30823
Naia Scott <nasc@microsoft.com>
2023-04-20 14:10:28 -0400

    Adding support for telemetry

---
+++ src/features/contracts.ts
@@ -0,0 +1,31 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the Source EULA. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+import { NotificationType } from "vscode-languageclient";
+import * as telemetry from '@microsoft/ads-extension-telemetry';
+
+
+// ------------------------------- < Telemetry Sent Event > ------------------------------------
+
+/**
+ * Event sent when the language service send a telemetry event
+ */
+
+export namespace TelemetryNotification {
+    export const type = new NotificationType<TelemetryParams, void>('telemetry/pgsqlevent');
+}
+
+/**
+ * Update event parameters
+ */
+
+export class TelemetryParams {
+    public params!: {
+        eventName: string;
+        properties: telemetry.TelemetryEventProperties;
+        measures: telemetry.TelemetryEventMeasures;
+    }
+}
+// ------------------------------- </ Telemetry Sent Event > ------------------------------------
