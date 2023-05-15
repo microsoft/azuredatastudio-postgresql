60f9691a5d76885908b850ed1d92c8fa11ea2008
Daeun Yim <daeunyim@microsoft.com>
2023-05-15 13:04:03 -0700

    use online packaging

--- tasks/packagetasks.js
+++ tasks/packagetasks.js
@@ -70,10 +70,27 @@ function doOfflinePackage(runtimeId, runtime, packageName) {
 
 //Install vsce to be able to run this task: npm install -g vsce
 gulp.task('package:online', () => {
-    return cleanServiceInstallFolder().then(() => {
-         doPackageSync();
-         return getOrDownloadServer();
+    var json = JSON.parse(fs.readFileSync('package.json'));
+    var name = json.name;
+    var version = json.version;
+    var packageName = name + '-' + version;
+
+    var packages = [];
+    packages.push({rid: 'win-x64', runtime: 'Windows_64'});
+    packages.push({rid: 'osx', runtime: 'OSX'});
+    packages.push({rid: 'osx-arm64', runtime: 'OSX_ARM64'});
+
+    var promise = cleanServiceInstallFolder();
+
+    packages.forEach(data => {
+        promise = promise.then(() => {
+            return cleanServiceInstallFolder().then(() => {
+                return doPackageSync(packageName + '-' + data.rid + '.vsix');
+            });
+        });
     });
+
+    return promise;
 });
 
 //Install vsce to be able to run this task: npm install -g vsce
