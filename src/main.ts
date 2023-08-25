072f9db763b8b3293242893c07f140e5191b2371
Joe Nelson <jonels@microsoft.com>
2023-08-25 10:26:23 -0500

    Add PG contextualization on extension activation
    ServerContextualizationServiceFeature

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,7 @@ import ContextProvider from './contextProvider';
 import * as Utils from './utils';
 import { TelemetryReporter, LanguageClientErrorHandler } from './telemetry';
 import { TelemetryFeature } from './features/telemetry';
+import { ServerContextualizationServiceFeature } from './features/context';
 
 const baseConfig = require('./config.json');
 const outputChannel = vscode.window.createOutputChannel(Constants.serviceName);
@@ -54,7 +55,8 @@ export async function activate(context: vscode.ExtensionContext) {
 		features: [
 			// we only want to add new features
 			...SqlOpsDataClient.defaultFeatures,
-			TelemetryFeature
+			TelemetryFeature,
+			ServerContextualizationServiceFeature
 		]
 	};
 
